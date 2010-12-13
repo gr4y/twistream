c0e15e6efd1cc7cfbd577f6926fb06e0702f310a
Sascha Wessel <swessel@gr4yweb.de>
2010-12-13 21:43:08 +0100

    removed debug code

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,7 +4,6 @@ describe TwiStream::Client do
 
   before :all do
     auth = YAML.load_file('spec/auth.yml')
-    puts auth.inspect
     @client = TwiStream::Client.new(auth)
     @client.on_error.handle do |msg,tr|
       puts msg 
